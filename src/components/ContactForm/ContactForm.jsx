0da9b327063eb7b139ce14a9867d335a74bdec4c
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-06-12 13:22:43 +0200

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,62 +1,68 @@
-import { Component } from "react";
-import { Form, Label, Input, ButtonAddDeleteContact } from "./ContactForm.styled";
+import { useState } from 'react';
+import {
+  Form,
+  Label,
+  Input,
+  ButtonAddDeleteContact,
+} from './ContactForm.styled';
 
-export class ContactForm extends Component{
-state = {
-name: '',
-number: '',
-}
+export default function ContactForm() {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-    
-handleChange = evt => {
-    const { name, value } = evt.currentTarget;
-    this.setState({[name]: value})
+  const handleChange = evt => {
+    const { name, value } = evt.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
     }
-    
-handleSubmit = evt => {
-    evt.preventDefault();
-    
-    this.props.onSubmit(this.state)
-    this.reset();
-    
-    }
+  };
 
+//   const handleSubmit = evt => {
+//     evt.preventDefault();
 
-reset = () => {
-    this.setState({ name: '', number: '' });
-}
-  
-render() {
-        const { name,number } = this.state;
-        
-        return (
-    <Form onSubmit={this.handleSubmit}>
-       <Label htmlFor="user_name">Name
+//     this.props.onSubmit(this.state);
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({ name: '', number: '' });
+//   };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Label htmlFor="user_name">
+        Name
         <Input
-  type="text"
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  required
-  value={name} 
-  onChange={this.handleChange} 
-                       
-            />
-        </Label>
-                
-        <Label htmlFor="user_tel">Number
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handleChange}
+        />
+      </Label>
+
+      <Label htmlFor="user_tel">
+        Number
         <Input
-  type="tel"
-  name="number"
-  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-  required
-  value={number} 
-  onChange={this.handleChange} 
-/>
-        </Label>
-        <ButtonAddDeleteContact type="submit">Add contact</ButtonAddDeleteContact>
-      
-    </Form> )
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={handleChange}
+        />
+      </Label>
+      <ButtonAddDeleteContact type="submit">Add contact</ButtonAddDeleteContact>
+    </Form>
+  );
 }
-};
